d4962274118444192aa80723a7f10d39bc860f6b
massomaster <massomaster24@gmail.com>
2026-09-22 10:03:04 +0200

    dbs

--- Backend/Src/Config/database.js
+++ Backend/Src/Config/database.js
@@ -1,7 +1,9 @@
 import mongoose from "mongoose";
 const connectDB = async () => {
     try {
-        
+        const connectionInstance = await mongoose.connect
+    ('${process.env.MONGODB_URI}') 
+      console.log
     } catch (error) {
         
     }
